287df00d8072772d58a3797887aa8535092ee7cb
KeithMartin1330 <30894820+KeithMartin1330@users.noreply.github.com>
2017-09-01 11:08:56 +0100

    Createing Project
    Creating Project

---
+++ maven-rest-api-project/src/main/java/com/keithmartin/app/App.java
@@ -0,0 +1,13 @@
+package com.keithmartin.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
